91d5c7aa2f571dcaf8d6e5c3dffb6bfbf963d826
adamkahil <adam.kahil@ontariotechu.net>
2020-10-03 16:51:18 -0400

    Add project files.

---
+++ GameObject.h
@@ -0,0 +1,24 @@
+#pragma once
+#ifndef __GAME_OBJECT__	
+#define __GAME_OBJECT__
+
+#include "PluginSettings.h"
+#include "Vector2D.h"
+
+class PLUGIN_API GameObject {
+
+public:
+	GameObject();
+
+	int GetID() const;
+	void SetID(int id = 0);
+
+	Vector2D GetPosition() const;
+	void SetPosition(float x = 0.0f, float y = 0.0f);
+
+private:
+	int m_id;
+	Vector2D m_position;
+};/////////////
+
+#endif
